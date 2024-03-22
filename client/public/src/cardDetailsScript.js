f9e367f55b435075d1200c90de72b5c6666a9cf9
newOlli <olli.lehmuskentta@gmail.com>
2024-03-23 00:05:33 +0800

    made cardDetail script better.

--- client/public/src/cardDetailsScript.js
+++ client/public/src/cardDetailsScript.js
@@ -1,5 +1,12 @@
+import {fetchCardDetails} from "../src/apiCalls.js"
 const queryParams = new URLSearchParams(window.location.search);
 const id = queryParams.get("id");
-const cardData = window.sessionStorage.getItem(id);
 
-document.getElementById("json-container").innerHTML = cardData;
+const cardDataResponse = await fetchCardDetails(id);
+if (cardDataResponse.status !== 200) {
+  console.log("error fetching card details");
+  //Maybe an icon or something here too?
+} else {
+  const cardDetailsJson = await cardDataResponse.json();
+  document.getElementById("json-container").innerHTML = JSON.stringify(cardDetailsJson);
+}
