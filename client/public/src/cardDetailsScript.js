2c8fc56eeda495b3f00bb2041d2fced1aef8d1da
Jiaqi Yang <jiayang@outlook.fr>
2024-03-29 21:35:21 +0800

    info about itenerary shows up

--- client/public/src/cardDetailsScript.js
+++ client/public/src/cardDetailsScript.js
@@ -9,4 +9,25 @@ if (cardDataResponse.status !== 200) {
 } else {
   const cardDetailsJson = await cardDataResponse.json();
   document.getElementById("json-container").innerHTML = JSON.stringify(cardDetailsJson);
+  document.getElementById("card-title").innerHTML = cardDetailsJson.title;
+  document.getElementById("card-description").innerHTML = cardDetailsJson.description
+  
+  for (var i = 0; i < cardDetailsJson.activities.length; i++) {
+    const newActivityDiv = document.createElement('div');
+    newActivityDiv.setAttribute('class', 'activity')
+
+    const newActivityName = document.createElement('text');
+    newActivityName.textContent = cardDetailsJson.activities[i].name;
+    newActivityName.setAttribute('class', 'activity-name');
+    newActivityDiv.appendChild(newActivityName);
+    newActivityDiv.appendChild(document.createElement('br'));
+
+    const newActivityDesc = document.createElement('text');
+    newActivityDesc.textContent = cardDetailsJson.activities[i].description;
+    newActivityDesc.setAttribute('class', 'activity-desc');
+    newActivityDiv.appendChild(newActivityDesc);
+    newActivityDiv.appendChild(document.createElement('br'));
+
+    document.body.appendChild(newActivityDiv);
+  }
 }
